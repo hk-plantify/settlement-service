e1053b67f7519c2f677f8de92cc237f46eebf3b2
zzoo0123 <0123suh@gmail.com>
2024-11-20 14:26:46 +0900

    feat: global exception 추가

---
+++ src/main/java/com/plantify/settlement/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.plantify.settlement.global.exception;
+
+import com.plantify.settlement.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ApiResponse<Void>> handleApplicationException(ApplicationException e) {
+        HttpStatus status = e.getHttpStatus();
+        ApiResponse<Void> response = ApiResponse.fail(status, e.getMessage());
+        return ResponseEntity.status(status).body(response);
+    }
+}
